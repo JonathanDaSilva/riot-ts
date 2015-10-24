5dee11ba9a677ad620c6aa3252521e1586b94568
Jonathan Da Silva <dasilvabastos.jonathan@gmail.com>
2015-10-25 00:15:47 +0200

    Update Definition

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
-export function template(template: string)
-export function tag(tagName: string)
+export declare function template(template: string)
+export declare function tag(tagName: string)
 
-export interface Riot {
-    private static __template: string
-    private static __tag: string
+export declare class Riot {
+    private static __template: string;
+    private static __tag: string;
 
-    protected root: HTMLElement
-    protected opts: any
+    protected root: HTMLElement;
+    protected opts: any;
 
-    public static register()
-    public static mount(tagName: string, args: Array<any>)
-    public update()
+    public static register();
+    public static mount(tagName: string, args: Object);
+    public update();
 }
